feab4f4b4f98219a6bd055a14257d90102459a76
Agnija <ilzena.agnija@gmail.com>
2021-09-02 11:03:18 +0200

    changes in server.js

--- backend/server.js
+++ backend/server.js
@@ -1,53 +1,27 @@
-require("dotenv").config();
-
 const express = require("express");
-const path = require("path");
+const cors = require("cors");
 
-//const ejs = require("ejs");
-const bodyParser = require("body-parser");
-
-
-//const MainController = require("./app/controllers/MainController"); 
 const PostsController = require("./app/controllers/PostsController");
-//const { Server } = require("http");
-//const PostsCtrl = new PostsController();
+const UserController = require("./app/controllers/UserController");
 
 const app = express();
 
-//set templating engine
-app.set("view engine", "ejs");
-// change default location of templating engine views
-app.set("views", path.resolve(__dirname, "app/views"));
+app.use(cors());
+app.options("*", cors());
+app.use(express.json());
 
-// exposed for users to fetch public
-app.use(express.static(path.resolve(__dirname,"assets")));
+//ROUTING
+//app.use("*", MainController);
 
-// use to parse req.body for POST, PUT etc requests.
-app.use(bodyParser.urlencoded({extended: false}));
-//app.use(express.urlencoded({extended: false} ));
+// blog POST routs
+app.post("/posts/create", PostsController.createPost);
+app.get("/posts", PostsController.getAllPosts);
 
+// blog USER roots
+app.post("/user/create", UserController.createUser);
 
-// ROUTING imported from controllers/MainController.js
-//app.use("*", MainController); 
-
-app.get("/", function(req,res) {
-    res.render("pages/index", {content: "Would you like some wine ?"});
-});
-//posts
-app.get("/posts/create", PostsController.renderForm);
-app.post("/posts/create", PostsController.create);
-app.get("/posts/:postId?", PostsController.main);
-app.post("/posts/delete/:postId?", PostsController.delete);
-app.post("/posts/:postId?", PostsController.update);
-//fallback to error page
-app.use("*", function(req, res) {
-    res.render("pages/error");
-});
-
-
-
-
-// SERVER LISTEN
-app.listen(3001, function () {
-    console.log("server is running on port 3001");
+// server listen
+const port = 3000;
+app.listen(port, function () {
+  console.log(`Server is running on: http://localhost:${port} .`);
 });
